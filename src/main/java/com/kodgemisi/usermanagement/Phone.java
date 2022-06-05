516730ebf19abbc48c5dbe4f2b8e88b0828c6760
KaganYgl <kaganyaylagulu@gmail.com>
2022-06-05 14:15:38 +0300

    Challenge #1 completed

--- src/main/java/com/kodgemisi/usermanagement/Phone.java
+++ src/main/java/com/kodgemisi/usermanagement/Phone.java
@@ -8,7 +8,17 @@ public class Phone implements CharSequence {
 	private final String phoneNumber;
 
 	public Phone(String phoneNumber) {
-		this.phoneNumber = phoneNumber == null ? "" : phoneNumber;
+		if(phoneNumber == null || phoneNumber.trim().equals("")){
+			throw new java.lang.IllegalArgumentException();
+		} else if(phoneNumber.equals("112") || phoneNumber.equals("911")){
+			this.phoneNumber = phoneNumber;
+		}else if(phoneNumber.charAt(0) != '+'){
+			throw new java.lang.IllegalArgumentException();
+		}else if(phoneNumber.length() < 5){
+			throw new java.lang.IllegalArgumentException();
+		}else if(phoneNumber.substring(1,3).equals("90") && phoneNumber.length() != 13){
+			throw new java.lang.IllegalArgumentException();
+		}else this.phoneNumber = phoneNumber;
 	}
 
 	public String number() {
